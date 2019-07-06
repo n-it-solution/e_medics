ff932ac82107e8c84a711923b1d4a30bf1c373ea
Abdul Manan <39721062+mirza339@users.noreply.github.com>
2019-07-06 17:09:42 +0500

    Initial commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="user")
+     */
+    public function index()
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    /**
+     * @Route("/user/register", name="user_register")
+     */
+    public function register()
+    {
+        return $this->render('user/register.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
